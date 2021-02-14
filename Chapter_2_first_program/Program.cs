95c96398c8664c2a1c47fccf09860f475f3f9c9e
BrazhnikDA <dimitry.brazhnik@yandex.ru>
2021-02-14 16:27:04 +0400

    Second_example_print_console

---
+++ Chapter_2_first_program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_2_first_program
+{
+    class Program
+    {
+        // Любая программа на C# начинается с вызова метода Main().
+        static void Main()
+        {
+            Console.WriteLine("Простая программа на С#.");
+            // Что бы не закрывалась консоль
+            Console.ReadKey();
+        }
+    }
+}
